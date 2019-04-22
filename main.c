0154b283add937c2b4f9775e462c860e4485c0b5
Matthew Grossman <mattig@udel.edu>
2019-04-21 23:00:53 -0400

    whoops accidentally edited main... fixed it!

--- main.c
+++ main.c
@@ -8,16 +8,14 @@ void sig_handler(int signal);
 int main( int argc, char **argv, char **envp )
 {
   /* put signal set up stuff here */
-  signal(SIGSTP, sig_handler);
+
 
   return sh(argc, argv, envp);
 }
 
 void sig_handler(int signal)
 {
-  if(sig == SIGSTP){
-    fflush(stdout);
-  }
+
   /* define your signal handler */
 }
 
